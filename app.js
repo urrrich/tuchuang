c410e29c5a81f4268caa469354697eb6d31b6f4f
sheruixu <493369342@qq.com>
2015-12-28 15:22:19 +0800

    test plupload

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var image = require('./routes/image');
+var users = require('./routes/users')
 
 var app = express();
 
 // view engine setup
-app.engine('.html',require('ejs').__express)
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.html',require('ejs').__express)
 app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/image', image);
+app.use('/users',users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/image.js
+++ routes/image.js
@@ -4,56 +4,62 @@ var router = express.Router();
 var Imagemin = require('imagemin')
 var formidable = require('formidable')
 var fs = require('fs')
+//tinypng
 /*
-router.post('/upload',function(req, res, next){   
-  var files = []
-  var form = formidable.IncomingForm()
-  form.keepExtensions = false
-  form.uploadDir = 'upload'
-  form.maxFieldsSize = 2*1024*1024
-  form.multiples = true
-
-  form.on('file', function(field, file) {
-    if(file.name){  
-      files.push(file)
-      //fs.renameSync(file.path,'upload/'+file.name)
-      new Imagemin().src(file.path).dest('upload/').run(function(err,files){ 
-        console.log(files[0])     
-      })
-    }else{  
-      fs.unlink(file.path)
-    }
-  })
-  form.on('progress',function(bytesReceived,bytesExpected){ 
-      
-  })
-  form.on('end',function(){ 
-    var result = {}
-    if(files.length){ 
-      result.message = "upload success"
-    }else{  
-      result.message = "please choose a pic"
-      result.error = true
-    }
-    res.send(result)
-  })
-  form.on('error',function(err){  
-
-  })
-  form.parse(req)
-})
+var tinify = require("tinify")
+tinify.key = 'GhNK2t4QbD1VelTSJxHzo1oNEVNYNteH'
 */
 
-router.post('/upload',function(req, res, next){     
+var uploadTempDir = 'upload_temp'
+var uploadDir = 'upload'
+
+router.post('/upload',function(req, res, next){ 
+
     var form = new formidable.IncomingForm();
     form.keepExtensions = true
-    form.uploadDir = "upload/";
+    form.uploadDir = uploadTempDir;
     form.parse(req, function(error, fields, files) {
+        if(error){  
+            return res.send({code:1})    
+        }
         var file = files.file
-        new Imagemin().src(file.path).dest('upload/').run(function(err,files){ 
-               
-        })
-  });
+        var dest = uploadDir
+        var output = file.path.replace(uploadTempDir,uploadDir)
+        
+        var imgmin = new Imagemin()
+        .src(file.path)
+        .dest(dest)
+        .use(Imagemin.jpegtran({progressive: true}))
+        .use(Imagemin.gifsicle())
+        .use(Imagemin.optipng({optimizationLevel: 3}))
+        .run(function(err, data){    
+            if(err){    
+                return res.send({code:1})                     
+            }
+            res.send({  
+                code:0,
+                input: {    
+                    size: file.size,
+                    type: file.type
+                },
+                output: {
+                    size: data[0].contents.length,
+                    type: file.type,
+                    name: file.name,
+                    url: output
+                }
+            })
+       })
+           
+    });
+})
+
+router.post('/download',function(req, res, next){ 
+    
+})
+
+router.post('/downloadzip',function(req, res, next){    
+    
 })
 
 module.exports = router;

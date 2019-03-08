ed2dca512dcbb3bba9eb4137f84a235dcc849257
小康 <baidu@google.com>
2019-03-08 11:22:41 +0800

    零时文件夹不存在,报错问题

--- routes/image.js
+++ routes/image.js
@@ -16,33 +16,36 @@ var uploadTempDir = 'upload_temp/'
 var uploadDir = staticDir + 'upload/'
 var zipDir = staticDir + 'zip/'
 
-router.post('/upload',function(req, res, next){ 
+router.post('/upload',function(req, res, next){
 
     var form = new formidable.IncomingForm();
     form.keepExtensions = true
+    if (!fs.existsSync(uploadTempDir)) {
+        fs.mkdirSync(uploadTempDir)
+    }
     form.uploadDir = uploadTempDir;
     form.parse(req, function(error, fields, files) {
-        if(error){  
-            return res.send({code:1})    
+        if(error){
+            return res.send({code:1})
         }
         var file = files.file
-        var output = file.path.replace(/\\/,'/') 
+        var output = file.path.replace(/\\/,'/')
         output = output.replace(uploadTempDir,uploadDir)
         output = output.replace(staticDir,'')
-        
+
         var imgmin = new Imagemin()
         .src(file.path)
         .dest(uploadDir)
         .use(Imagemin.jpegtran({progressive: true}))
         .use(Imagemin.gifsicle())
         .use(Imagemin.optipng({optimizationLevel: 3}))
-        .run(function(err, data){    
-            if(err){    
-                return res.send({code:1})                     
+        .run(function(err, data){
+            if(err){
+                return res.send({code:1})
             }
-            res.send({  
+            res.send({
                 code:0,
-                input: {    
+                input: {
                     size: file.size,
                     type: file.type
                 },
@@ -54,14 +57,14 @@ router.post('/upload',function(req, res, next){
                 }
             })
        })
-           
+
     });
 })
 
-router.get('/download',function(req, res, next){ 
+router.get('/download',function(req, res, next){
     var q = req.query
     var filepath = q.p
-    var filename = q.n 
+    var filename = q.n
     if(filename && filepath){
         res.setHeader('Content-disposition', 'attachment; filename=' + filename)
         var filestream = fs.createReadStream(staticDir + filepath)
@@ -71,17 +74,17 @@ router.get('/download',function(req, res, next){
         filestream.on('end', function() {
             res.end()
         });
-    }else{  
-        res.send('check your params')        
+    }else{
+        res.send('check your params')
     }
 })
 
-router.post('/downloadzip',function(req, res, next){ 
-    var files = JSON.parse(req.body.q) 
+router.post('/downloadzip',function(req, res, next){
+    var files = JSON.parse(req.body.q)
     var zippath = zipDir + Date.now() + '.zip'
     var output = fs.createWriteStream(zippath)
     var archive = archiver('zip')
-    archive.on('end',function(){  
+    archive.on('end',function(){
         res.send({
             code: 0,
             url: zippath.replace(staticDir,'')
@@ -89,7 +92,7 @@ router.post('/downloadzip',function(req, res, next){
     })
     archive.pipe(output)
     files.forEach(function(e){
-        archive.append(fs.createReadStream(staticDir + e.p),{name:e.n})   
+        archive.append(fs.createReadStream(staticDir + e.p),{name:e.n})
     })
     archive.finalize()
 })
